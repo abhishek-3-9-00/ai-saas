d19b254425b7a20d992ad993272749663d7e87ae
abhishek-awako <abhishekv3900@gmail.com>
2023-11-19 23:38:30 +0530

    stripe route

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -10,49 +10,55 @@ export async function GET() {
   try {
     const { userId } = auth();
     const user = await currentUser();
+
+    // Unauthorized if user not authenticated
     if (!userId || !user) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+    
+    // Fetch user's subscription status
     const userSubscription = await prismadb.userSubscription.findUnique({
-      where: {
-        userId,
-      },
+      where: { userId },
     });
+
+    // Handling Stripe session creation based on subscription status
+    let stripeSession;
     if (userSubscription && userSubscription.stripeCustomerId) {
-      const stripeSession = await stripe.billingPortal.sessions.create({
+      stripeSession = await stripe.billingPortal.sessions.create({
         customer: userSubscription.stripeCustomerId,
         return_url: settingUrl,
       });
-      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+    } else {
+      stripeSession = await stripe.checkout.sessions.create({
+        success_url: settingUrl,
+        cancel_url: settingUrl,
+        payment_method_types: ["card"],
+        mode: "subscription",
+        billing_address_collection: "auto",
+        customer_email: user.emailAddresses[0].emailAddress,
+        line_items: [
+          {
+            price_data: {
+              currency: "INR",
+              product_data: {
+                name: "ImaginAI",
+                description: "Unlimited AI Generations",
+              },
+              unit_amount: 5000,
+              recurring: { interval: "month" },
+            },
+            quantity: 1,
+          },
+        ],
+        metadata: { userId },
+      });
     }
 
-    const stripeSession = await stripe.checkout.sessions.create({
-      success_url: settingUrl,
-      cancel_url: settingUrl,
-      payment_method_types: ["card"],
-      mode: "subscription",
-      billing_address_collection: "auto",
-      customer_email: user.emailAddresses[0].emailAddress,
-      line_items: [
-        {
-          price_data: {
-            currency: "INR",
-            product_data: {
-              name: "ImaginAI",
-              description: "Unlimited AI Generations",
-            },
-            unit_amount: 5000,
-            recurring: {
-              interval: "month",
-            },
-          },
-          quantity: 1,
-        },
-      ],
-      metadata: {
-        userId,
-      },
-    });
+    // Return the Stripe session URL
     return new NextResponse(JSON.stringify({ url: stripeSession.url }));
-  } catch (error) {}
+  } catch (error) {
+    // Handle errors and return a response
+    console.error("Error occurred:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
